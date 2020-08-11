b0f2bdedb44173e0817c0dd2ddbf1d2d4be7883b
Lucas Bigelow <lukejbigelow@gmail.com>
2020-08-11 14:14:04 -0600

    added basic genre search with case sensitivity

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
 'use strict';
 
+require('dotenv').config();
 const express = require('express');
+const movies = require('./movies.json');
 
 const app = express();
+
+const validateBearerToken = (req, res, next) => {
+  const authToken = req.get('Authorization') || '';
+  const apiToken = process.env.API_TOKEN;
+
+  if(!authToken || authToken.split(' ').pop() !== apiToken) {
+    res.status(401).json({error: 'Unauthorized user, access denied.'});
+  }
+
+  next();
+};
+
+app.use(validateBearerToken);
+
+const handleSearchMovies = (req, res) => {
+  const {genre, country, avg_vote} = req.query; 
+  
+  let searchedMovies = [...movies];
+
+  if(genre) {
+    searchedMovies = searchedMovies.filter(movie => {
+      return movie.genre === genre;
+    });
+  }
+
+  res.json(searchedMovies);
+};
+
+app.get('/movies', handleSearchMovies);
 
 app.listen(8000, () => console.log('Server on port 8000'));
