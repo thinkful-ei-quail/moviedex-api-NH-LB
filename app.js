122578352265d87909f129d4d3b44e624a48e7b1
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-08-11 13:37:15 -0700

    add Number to movieavgvote

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ const handleSearchMovies = (req, res) => {
 
   if(avg_vote) {
     searchedMovies = searchedMovies.filter(movie => {
-      return movie.avg_vote >= Number(avg_vote);
+      return Number(movie.avg_vote) >= Number(avg_vote);
     })
   }
 
